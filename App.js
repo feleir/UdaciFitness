6ed472c3335479c9e129c29d6369a1231f2b869d
Ivan Diaz <feleir@gmail.com>
2018-01-24 11:45:35 +0100

    Update background colour to black to be able to see the icons.

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
-import { View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import AddEntry from './components/AddEntry'
 
 export default class App extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.container}>
           <AddEntry />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+})
