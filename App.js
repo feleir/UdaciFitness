06528989c949b15e20b4db7a4f6f9cd1c918bb78
Ivan Diaz <feleir@gmail.com>
2018-01-25 10:04:10 +0100

    Added stepper and slider components

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
