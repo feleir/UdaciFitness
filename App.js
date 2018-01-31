7233b1a8f706c1200e51119b1a03e2c19f84428a
Ivan Diaz <feleir@gmail.com>
2018-01-31 10:01:16 +0100

    Added redux and react-redux

--- App.js
+++ App.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import { StyleSheet, View } from 'react-native'
 import AddEntry from './components/AddEntry'
 
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-          <AddEntry />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+            <AddEntry />
+        </View>
+      </Provider>
     );
   }
 }
